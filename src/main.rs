23b59572f2c40e60cfdbc8b6a3cbb1d2b9dd3d14
lise <18689574+43mm@users.noreply.github.com>
2025-08-21 16:24:28 +1000

    feat: fetch drops response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    println!("fetching open drop campaigns...");
+
+    let json_string  = reqwest::blocking::get("https://twitch-drops-api.sunkwi.com/drops")?.text()?;
+    println!("fetched: {}", &json_string[..500.min(json_string.len())]);
+
+    Ok(())
 }
