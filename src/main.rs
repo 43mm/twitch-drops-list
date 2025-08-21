b34e7e11d5d3fda4c95047dc280fab2e59ca76d5
lise <18689574+43mm@users.noreply.github.com>
2025-08-21 23:16:54 +1000

    feat: add api data models and parse json

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
+use serde::Deserialize;
 use std::error::Error;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct ApiGame {
+    game_display_name: String,
+    #[serde(rename = "rewards")]
+    drops: Vec<ApiDrops>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct ApiDrops {
+    name: String,
+    start_at: String,
+    end_at: String,
+    #[serde(rename = "timeBasedDrops")]
+    rewards: Vec<ApiReward>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct ApiReward {
+    name: String,
+    required_minutes_watched: u16,
+}
 
 fn main() -> Result<(), Box<dyn Error>>{
     println!("fetching open drop campaigns...");
 
     let json_string  = reqwest::blocking::get("https://twitch-drops-api.sunkwi.com/drops")?.text()?;
-    println!("fetched: {}", &json_string[..500.min(json_string.len())]);
+    let api_data: Vec<ApiGame> = serde_json::from_str(&json_string)?;
 
     Ok(())
 }
